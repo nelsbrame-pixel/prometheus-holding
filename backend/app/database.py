d169612b98fb66dc52ae2d8e52c430a075e7d849
Nelson <nelsbrame@gmail.com>
2026-04-29 13:35:51 -0300

    corrige database url render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("postgres://"):
+# Corrige postgres:// para postgresql://
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
