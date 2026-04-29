1c6915f27826d7b8508677ac59bb45edd6a37870
Nelson <nelsbrame@gmail.com>
2026-04-29 18:00:08 -0300

    salva usuario no postgres

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from .database import engine, Base
 from .models.user import User
+from .schemas import UserCreate
 
 app = FastAPI(title="PROMETHEUS CORE")
 
 Base.metadata.create_all(bind=engine)
 
+
 @app.get("/health")
 def health():
     return {"status": "online"}
+
+
+@app.post("/register")
+def register(user: UserCreate):
+    return {
+        "message": "Usuário recebido com sucesso",
+        "email": user.email
+    }
